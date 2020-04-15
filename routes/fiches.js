e96cd0914f7a885ec7eda1b3c74d1353558ccdf0
Nicolas <Nicolas@MacBook-Nie.local>
2020-04-15 22:40:04 +0200

    Ajout d'une fiche

--- routes/fiches.js
+++ routes/fiches.js
@@ -12,7 +12,7 @@ router
   .route("/")
 
   //NIE GET monurl/fiches retoune la liste des fiches.
-  .get(function(req, res) {
+  .get(function (req, res) {
     //est ce que j'ai un paramètre?
     var texteRecherche = req.query.recherche;
 
@@ -22,35 +22,41 @@ router
       texteRecherche = texteRecherche.replace(/ /gi, "|");
       texteRecherche = new RegExp(texteRecherche, "i");
       filtreRecherche = {
-        $or: [{ titre: texteRecherche }, { description: texteRecherche }]
+        $or: [{ titre: texteRecherche }, { description: texteRecherche }],
       };
       console.log(filtreRecherche);
     }
 
-    fiches.find(filtreRecherche, "_id titre description", function(
-      err,
-      fiches
-    ) {
-      if (err) {
-        res.send(err);
-      }
-      res.json(fiches);
-    });
+    fiches
+      .find(filtreRecherche, "_id titre description", function (err, fiches) {
+        if (err) {
+          res.send(err);
+        }
+        res.json(fiches);
+      })
+      .sort({ titre: 1 });
   })
 
   //NIE POST permet d'ajouter une fiche
-  .post(function(req, res) {
+  .post(function (req, res) {
     var maFiche = new fiches();
     maFiche.titre = req.body.titre;
+    maFiche.titre_fiche = req.body.titre_fiche;
+    maFiche.description = req.body.description;
+    maFiche.date_modif = new Date();
+    maFiche.symptomes = req.body.symptomes;
+    maFiche.conseils = req.body.conseils;
+    maFiche.aller_chez_le_medecin = req.body.aller_chez_le_medecin;
+    maFiche.articles = req.body.articles;
 
     // Insertion dans la collection fiches de la base MongoDB
-    maFiche.save(function(err) {
+    maFiche.save(function (err) {
       if (err) {
         res.send(err);
       }
       res.send({
         message: "Bravo, la fiche  est maintenant stockée en base de données",
-        id: maFiche.id
+        id: maFiche.id,
       });
     });
   });
@@ -59,11 +65,42 @@ router
 router
   .route("/:fiche_id")
 
-  .get(function(req, res) {
-    fiches.findById(req.params.fiche_id, function(err, fiches) {
-      if (err) res.send(err);
-      res.json(fiches);
+  .get(function (req, res) {
+    fiches.findById(req.params.fiche_id, function (err, fiches) {
+      if (err) {
+        res.send(err);
+        console.log(err);
+      } else {
+        res.json(fiches);
+      }
     });
+  })
+
+  .patch(function (req, res) {
+    var data = {
+      titre: req.body.titre,
+      titre_fiche: req.body.titre_fiche,
+      description: req.body.description,
+      date_modif: new Date(),
+      symptomes: req.body.symptomes,
+      conseils: req.body.conseils,
+      aller_chez_le_medecin: req.body.aller_chez_le_medecin,
+      articles: req.body.articles,
+    };
+
+    fiches.findByIdAndUpdate(
+      req.params.fiche_id,
+      data,
+      { new: true },
+      function (err, fiches) {
+        if (err) {
+          res.send(err);
+          console.log(err);
+        } else {
+          res.json(fiches);
+        }
+      }
+    );
   });
 
 // exporte le module routeur decrit dans ce fichier
